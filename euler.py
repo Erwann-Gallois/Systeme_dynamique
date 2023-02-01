2278cab8e18348a3c01790f38a20165003b0c393
Erwann_Gallois <78953327+Erwann-Gallois@users.noreply.github.com>
2023-02-01 16:24:10 +0000

    premiere_version
    Signed-off-by: Erwann_Gallois <78953327+Erwann-Gallois@users.noreply.github.com>

--- euler.py
+++ euler.py
@@ -1,1 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+def flg(y, t):
+    r = 3
+    K = 1
+    return r*y*(1-(y/K))
+# t -> liste des temps donné
+def euler (y0, f, t):
+    N = len(t)
+    y = [0] * N
+    y[0] = y0
+    print(y)
+    for i in range(1, N):
+        y[i] = y[i-1] + (t[i]-t[i-1])*f(y[i-1], t[i-1])
+    return y
+
+print(euler(0.1, flg, [0,1,2,3,4,5,6,7,8,9,10]))
