f647b577a1b33d1e226effe859013a3e53681f13
Erwann_Gallois <78953327+Erwann-Gallois@users.noreply.github.com>
2023-02-01 16:43:34 +0000

    marche_mais_brouillon
    Signed-off-by: Erwann_Gallois <78953327+Erwann-Gallois@users.noreply.github.com>

--- euler.py
+++ euler.py
@@ -1,18 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def flg(y, t):
+def flg(N,t):
     r = 3
     K = 1
-    return r*y*(1-(y/K))
-# t -> liste des temps donné
-def euler (y0, f, t):
-    N = len(t)
-    y = [0] * N
+    return r*N*(1-(N/K))
+
+def Euler(h, n, t0, y0, f):
+
+    t = [0] * (int(n/h)+1)
+    t[0] = t0
+
+    y = [0] * len(t)
     y[0] = y0
-    print(y)
-    for i in range(1, N):
-        y[i] = y[i-1] + (t[i]-t[i-1])*f(y[i-1], t[i-1])
+
+    for i in range(1,len(t)):
+        t[i] = t[i-1] + h
+        y[i] = y[i-1] + (t[i]-t[i-1]) * f(y[i-1], t[i-1]) 
     return y
 
-print(euler(0.1, flg, [0,1,2,3,4,5,6,7,8,9,10]))
+print(Euler(0.1, 10, 0, 0.1, flg))
